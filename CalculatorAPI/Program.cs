5e6b419c9bc8d4c506f874718c76fbf7aa9902ce
A00299577 <A00299577@mycambrian.ca>
2024-10-07 19:07:35 +0000

    registered services in program.cs file

--- CalculatorAPI/Program.cs
+++ CalculatorAPI/Program.cs
@@ -1,6 +1,10 @@
+using CalculatorAPI.Services;
+using CalculatorAPI.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ICalculatorService, CalculatorService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
